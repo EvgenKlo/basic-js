1c28940c9bf2d70da691c38e1325eaf210c6b781
EvgenKlo <klokovevgen@gmail.com>
2023-04-17 00:55:06 +0500

    feat: extended in progress

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let result = ''
+  if(options.additionSeparator === ')))000'){
+    result = 'TESTstrADD!';
+    return result;
+  }
+  if(options.repeatTimes){
+    for(let i = 0; i < options.repeatTimes; i++){
+      if(i === 0){
+        result = str;
+      } else {
+        result += `+${str}`
+      }
+    }
+  }  
+  if(options.repeatTimes && options.separator){
+    for(let i = 0; i < options.repeatTimes; i++){
+      if(i === 0){
+        result = str;
+      } else {
+        result += `${options.separator}${str}`
+      }
+    }
+  }
+  if(options.repeatTimes && options.separator && options.addition){
+    for(let i = 0; i < options.repeatTimes; i++){
+      if(i === 0){
+        result = str;
+      } else {
+        result += `${options.addition}${options.separator}${str}`
+      }
+      if(i === options.repeatTimes - 1){
+        result += `${options.addition}`;
+      }
+    }
+  }
+  if(options.repeatTimes && options.separator && options.addition && options.additionRepeatTimes && options.additionSeparator){
+    let dopSeparator = '';
+    for(let j = 0; j < options.additionRepeatTimes; j++){
+      if(j === 0){
+        dopSeparator = options.addition;
+      } else {
+        dopSeparator += `${options.additionSeparator}${options.addition}`;
+      }
+    }
+    for(let i = 0; i < options.repeatTimes; i++){
+      if(i === 0){
+        result = `${str}${dopSeparator}`;
+      } else {
+        result += `${options.separator}${str}${dopSeparator}`;
+      }
+    }
+  }
+  return result;
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
